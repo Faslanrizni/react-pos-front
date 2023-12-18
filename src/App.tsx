e1f406649a578af9f0b21eaf11fa1dd5c21b8be4
Faslan Rizni <Faslan2002Rizni@gmail.com>
2023-12-18 21:12:15 +0530

    Router, Routes applied

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,39 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import {BrowserRouter as Routes, Link, Route, Router} from "react-router-dom";
 import './App.css'
 
 function App() {
 
 
   return (
-    <h1>Hello world</h1>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to={'/'}>Home</Link>
+            </li>
+            <li>
+              <Link to={'/customer'}>Customer</Link>
+            </li>
+            <li>
+              <Link to={'/product'}>Product</Link>
+            </li>
+          </ul>
+        </nav>
+        <hr/>
+
+        <Routes>
+          <Route path={'/'} element={<Home/>} />
+          <Route path={'/customer'} element={<Customer/>} />
+          <Route path={'/product'} element={<Product/>} />
+        </Routes>
+        
+      </div>
+    </Router>
+
   )
 }
 
