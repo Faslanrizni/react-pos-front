9b2efe87cbb68670990df7ae6d8487ed82a8a0fc
Faslan Rizni <Faslan2002Rizni@gmail.com>
2023-12-19 10:51:46 +0530

    card added via component based completed

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -8,17 +8,41 @@ function Home() {
             <br/>
             <div className="container">
                 <div className="row">
-                    <div className="col-12 col-sm-6 col-md-4 col-l-3">
-                        <DefaultCard/>
+                    <div className="col-12 col-sm-6 col-md-4 col-lg-3">
+                        <DefaultCard
+                            thumbnail={'https://img.freepik.com/free-photo/focused-business-group-studying-report_1262-21043.jpg?size=626&ext=jpg&ga=GA1.1.1313303609.1697107729&semt=sph' }
+                            description={'Customer'}
+                            title={'Customer'}
+                            value={2750}
+                            key={1}
+                        />
                     </div>
-                    <div className="col-12 col-sm-6 col-md-4 col-l-3">
-                        <DefaultCard/>
+                    <div className="col-12 col-sm-6 col-md-4 col-lg-3">
+                        <DefaultCard
+                            thumbnail={'https://img.freepik.com/premium-photo/large-clean-warehouse-with-shelves-cardboard-boxes-products-generative-ai_1005220-1640.jpg?size=626&ext=jpg&ga=GA1.2.1313303609.1697107729&semt=sph'}
+                            description={'Products'}
+                            title={'Products'}
+                            value={2750}
+                            key={2}
+                        />
                     </div>
-                    <div className="col-12 col-sm-6 col-md-4 col-l-3">
-                        <DefaultCard/>
+                    <div className="col-12 col-sm-6 col-md-4 col-lg-3">
+                        <DefaultCard
+                            thumbnail={'https://img.freepik.com/free-photo/medium-shot-man-drinking-coffee_23-2148539831.jpg?size=626&ext=jpg&ga=GA1.1.1313303609.1697107729&semt=sph'}
+                            description={'Orders'}
+                            title={'Orders'}
+                            value={2750}
+                            key={3}
+                        />
                     </div>
-                    <div className="col-12 col-sm-6 col-md-4 col-l-3">
-                        <DefaultCard/>
+                    <div className="col-12 col-sm-6 col-md-4 col-lg-3">
+                        <DefaultCard
+                            thumbnail={'https://img.freepik.com/free-photo/business-man-counting-dollar-banknote-online-business-concept_1150-6406.jpg?size=626&ext=jpg&ga=GA1.2.1313303609.1697107729&semt=sph'}
+                            description={'income'}
+                            title={'income'}
+                            value={2750}
+                            key={4}
+                        />
                     </div>
 
                 </div>
